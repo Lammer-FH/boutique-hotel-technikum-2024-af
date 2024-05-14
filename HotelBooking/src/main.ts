b27df034c0fb066966c9baa32b167a75822d38e5
Stefan <simanek.s@gmx.at>
2024-05-14 23:37:16 +0200

    added ionic/vue

--- HotelBooking/src/main.ts
+++ HotelBooking/src/main.ts
@@ -2,13 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { IonicVue } from '@ionic/vue'
 
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const app = createApp(App).use(IonicVue).use(router);
 
 app.use(createPinia())
 app.use(router)
 
-app.mount('#app')
+router.isReady().then(() => {
+    app.mount('#app')
+});
+
+
