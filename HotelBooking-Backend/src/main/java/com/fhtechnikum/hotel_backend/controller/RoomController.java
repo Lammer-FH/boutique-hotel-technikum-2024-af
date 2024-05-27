9f82c87eee5926bf8857724151b1691c7be648a2
Wojciech Lata <wojciech.r.lata@gmail.com>
2024-05-28 00:32:24 +0200

    added per-id room query

--- HotelBooking-Backend/src/main/java/com/fhtechnikum/hotel_backend/controller/RoomController.java
+++ HotelBooking-Backend/src/main/java/com/fhtechnikum/hotel_backend/controller/RoomController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/rooms")
@@ -33,6 +36,16 @@ public class RoomController {
         response.put("rooms", resultFromService);
         return response;
         //return resultFromService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Room> getRoomById(@PathVariable Integer id) {
+        Optional<Room> room = service.getRoomById(id);
+        if (room.isPresent()) {
+            return ResponseEntity.ok(room.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 
--- HotelBooking-Backend/src/main/java/com/fhtechnikum/hotel_backend/service/RoomService.java
+++ HotelBooking-Backend/src/main/java/com/fhtechnikum/hotel_backend/service/RoomService.java
@@ -4,6 +4,7 @@ import com.fhtechnikum.hotel_backend.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoomService {
@@ -12,6 +13,10 @@ public class RoomService {
 
     public Iterable<Room> getAllRooms(){
         return roomRepository.findAll();
+    }
+
+    public Optional<Room> getRoomById(Integer id) {
+        return roomRepository.findById(id);
     }
 
 }
