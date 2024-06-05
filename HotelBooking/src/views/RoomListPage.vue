5cb1ad7ceb3eef758b97ba1cf2082791a90ec67e
Stefan <simanek.s@gmx.at>
2024-06-05 08:55:14 +0200

    Enforced modularity with strict types for roomlist and extraslist

---
+++ HotelBooking/src/views/RoomListPage.vue
@@ -0,0 +1,60 @@
+<template>
+    <ion-list>
+        <ion-item v-for="(room, index) in paginatedRooms" :key="room.id" @click="selectRoom(room.id)">
+            <img :src="getImagePath(index)" alt="Room Image" class="room-image" />
+            <ion-checkbox slot="start" :checked="selectedRoomId === room.id"></ion-checkbox>
+            <ion-label>
+                <h3>{{ room.title }}</h3>
+                <p>{{ room.description }}</p>
+                <ExtrasList :extras="room.extras" />
+            </ion-label>
+        </ion-item>
+    </ion-list>
+</template>
+
+<script lang="ts">
+import { IonList, IonItem, IonLabel, IonCheckbox } from '@ionic/vue';
+import ExtrasList from './ExtrasListPage.vue';
+
+export default {
+    name: 'RoomList',
+    components: {
+        IonList,
+        IonItem,
+        IonLabel,
+        IonCheckbox,
+        ExtrasList
+    },
+    props: {
+        rooms: {
+            type: Array as () => { id: number; title: string; description: string; extras: string[] }[],
+            required: true
+        },
+        paginatedRooms: {
+            type: Array as () => { id: number; title: string; description: string; extras: string[] }[],
+            required: true
+        },
+        selectedRoomId: {
+            type: Number,
+            default: null
+        }
+    },
+    emits: ['selectRoom'],
+    methods: {
+        getImagePath(index: number) {
+            return `/Rooms/room${index + 1}.jpg`;
+        },
+        selectRoom(roomId: number) {
+            this.$emit('selectRoom', roomId);
+        }
+    }
+};
+</script>
+
+<style scoped>
+.room-image {
+    width: 100px;
+    height: auto;
+    margin-right: 10px;
+}
+</style>
