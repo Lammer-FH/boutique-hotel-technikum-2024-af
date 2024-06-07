fd9029a8f7e3c4a99648c06b11846a218e2af90c
Stefan <simanek.s@gmx.at>
2024-06-08 01:04:38 +0200

    added pinia and empty store

--- HotelBooking/src/main.ts
+++ HotelBooking/src/main.ts
@@ -26,9 +26,13 @@ import '@ionic/vue/css/palettes/dark.system.css';
 /* Theme variables */
 import './theme/variables.css';
 
+/* Import Pinia */
+import { createPinia } from 'pinia';
+
 const app = createApp(App)
   .use(IonicVue)
-  .use(router);
+  .use(router)
+  .use(createPinia());
 
 router.isReady().then(() => {
   app.mount('#app');
