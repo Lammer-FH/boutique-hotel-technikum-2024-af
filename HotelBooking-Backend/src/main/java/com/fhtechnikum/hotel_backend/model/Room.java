c6e4151f01037302b78f060447d8f3a35a0362a6
Nicolas Klement <nicolas.klement@gmail.com>
2024-06-14 22:24:32 +0200

    Include room extras as part of room query

--- HotelBooking-Backend/src/main/java/com/fhtechnikum/hotel_backend/model/Room.java
+++ HotelBooking-Backend/src/main/java/com/fhtechnikum/hotel_backend/model/Room.java
@@ -2,6 +2,8 @@ package com.fhtechnikum.hotel_backend.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Set;
+
 @Entity(name = "ROOM")
 @Table(name = "ROOM")
 @Getter
@@ -11,8 +13,16 @@ public class Room {
     @Id
     @Column(name = "ROOM_ID")
     private int roomId;
+
     @Column(name = "ROOM_TITLE")
     private String roomTitle;
+
     @Column(name = "ROOM_DESCRIPTION")
     private String roomDescription;
+
+    @ManyToMany(targetEntity = Extra.class, cascade = CascadeType.ALL)
+    @JoinTable(name = "ROOM_EXTRA",
+            joinColumns = { @JoinColumn(name = "ROOM_ID_FK") },
+            inverseJoinColumns = { @JoinColumn(name = "EXTRA_ID_FK") })
+    private Set<Extra> roomExtras;
 }
